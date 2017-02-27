96a2f95ba6a91fb714012d69d4f6822f8d292a4d
liqwid <Anton.Popov@infowatch.com>
2017-02-27 23:46:27 +0300

    fixed jsnext build;

--- rollup.config.es6.js
+++ rollup.config.es6.js
@@ -1,6 +1,6 @@
 import config from './rollup.config';
 
 config.format = 'es6';
-config.dest = 'dist/localforage-plugin-boilerplate.es6.js';
+config.dest = 'dist/localforage-indexes.es6.js';
 
 export default config;
